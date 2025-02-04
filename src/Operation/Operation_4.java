c10bf1233f723d84e707acab8447de77db1d8002
rushikesh452 <kalerushikesh8701@gmail.com>
2025-02-05 00:07:22 +0530

    Operation_4 commit

--- src/Operation/Operation_4.java
+++ src/Operation/Operation_4.java
@@ -5,6 +5,10 @@ public class Operation_4 {
     public static void main(String[] args) {
 
         int age=50;
+        int age1=40;
         System.out.println(age);
+        System.out.println(age1);
+
+        System.out.println(age+age1);
     }
 }
